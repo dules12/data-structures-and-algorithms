9f3819d7a395db797e0d5fbe134b6513b0bfb12b
dules12 <danielgeo@comcast.net>
2018-11-29 09:18:16 -0800

    done with 2 and attempted 3

--- code-challenges/301/regEx2/challenges-11.test.js
+++ code-challenges/301/regEx2/challenges-11.test.js
@@ -55,9 +55,9 @@ Your function should include a single regular expression pattern that matches an
 Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
-const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
-};
+// const validatePhoneNumber = (phoneNumber) => {
+//   ^(\(|\d{3}\)\s\d{3}\-\d{4})
+// };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4 - Stretch Goal
